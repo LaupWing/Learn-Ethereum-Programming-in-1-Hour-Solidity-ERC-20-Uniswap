9914382c71bff4aa9e2c18db8c48516ea66ca67c
LaupWing <loc_strict@hotmail.com>
2022-04-08 16:03:20 +0200

    remix

--- Token.sol
+++ Token.sol
@@ -7,6 +7,10 @@ contract Token {
    uint256 public decimals;
    uint256 public totalSupply;
 
+   mapping(address => uint256) public balanceOf;
+
+   event Transfer(address indexed from, address indexed to, uint256 value);
+
    constructor(
       string memory _name,
       string memory _symbol,
@@ -17,5 +21,15 @@ contract Token {
       symbol = _symbol;
       decimals = _decimals;
       totalSupply = _totalSupply;
+      balanceOf[msg.sender] = totalSupply;
+   }
+   function transfer(address _to, uint256 _value) external returns (bool success){
+      require(balanceOf[msg.sender] >= _value);
+
+      balanceOf[msg.sender] = balanceOf[msg.sender] - (_value);
+      balanceOf[_to] = balanceOf[_to] + (_value);
+
+      emit Transfer(msg.sender, _to, _value);
+      return true;
    }
 }
