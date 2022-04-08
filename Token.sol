11393f8cb87082d9df1cb258d3f2db67e293a99d
LaupWing <loc_strict@hotmail.com>
2022-04-08 15:45:58 +0200

    contructor

--- Token.sol
+++ Token.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract Token{
-    string public name = "My Token";
+contract Token {
+   string public name;
+   string public symbol;
+   uint256 public decimals;
+   uint256 public totalSupply;
 
-    
+   constructor(
+      string memory _name,
+      string memory _symbol,
+      uint256 _decimals,
+      uint256 _totalSupply
+   ) {
+      name = _name;
+      symbol = _symbol;
+      decimals = _decimals;
+      totalSupply = _totalSupply;
+   }
 }
